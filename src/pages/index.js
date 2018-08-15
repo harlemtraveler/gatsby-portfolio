2b453304aa2dbc7c86ba91ab50b98b537f6051ce
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-08-15 08:34:22 -0400

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = ({ data }) => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+    {}
+  </div>
+)
+export const query = graphql`
+  query BannerImage{
+    allImageSharp {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage
